e41d0496951f82a05c08f507e93dc131035bb92c
hmhm903 <hmhm903m@gmail.com>
2017-07-08 19:43:12 +0900

    Organize the code of rendering display.

--- js/boids.js
+++ js/boids.js
@@ -1,13 +1,15 @@
 var init = function()
 {
   renderer = new THREE.WebGLRenderer();
-  const kInitialLength = 600;
-  renderer.setSize(kInitialLength, kInitialLength);
+
+  var size = getWindowSize();
+  container.style.cssText = "width: " + size.width +"px; height: " + size.height + "px;";
+  renderer.setSize(size.width, size.height);
   container = document.getElementById("container");
   container.appendChild(renderer.domElement);
 
   var scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(45, kInitialLength / kInitialLength, 1, 1000);
+  camera = new THREE.PerspectiveCamera(45, 1.0, 1, 1000);
 
   var geometry = new THREE.BoxGeometry(1,1,1);
   var material = new THREE.MeshPhongMaterial({color: 0x00ffff});
@@ -25,10 +27,6 @@ var init = function()
     requestAnimationFrame(update);
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
-
-    light.position.x = 1.0 * Math.cos(cnt*2 / 180 * Math.PI);
-    light.position.y = 1.0 * Math.sin(cnt*2 / 180 * Math.PI);
-
     cnt++;
     renderer.render(scene,camera);
   }
@@ -37,15 +35,6 @@ var init = function()
 }
 
 // full screen: reference: http://www.inazumatv.com/contents/archives/8484
-var MIN_WIDTH = 1280, MIN_HEIGHT = 640;
-filterWindowSize = function (size){
-    var w = size.width, h = size.height;
-    return {
-        width: w <  MIN_WIDTH ? MIN_WIDTH : w,
-        height: h < MIN_HEIGHT ? MIN_HEIGHT: h
-    };
-};
-
 getWindowSize = function () {
     return {
         width: window.innerWidth,
@@ -55,9 +44,7 @@ getWindowSize = function () {
 
 onWindowResize = function()
 {
-  var size = filterWindowSize(getWindowSize());
-  console.log(size);
-
+  var size = getWindowSize();
   container.style.cssText = "width: " + size.width +"px; height: " + size.height + "px;";
   renderer.setSize(size.width, size.height);
 
